f3b22b7e7db84401e9270438b92a052784497225
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-11-21 22:30:04 -0600

    Update 225 ques p3.py

--- 225 ques p3.py
+++ 225 ques p3.py
@@ -2,12 +2,16 @@
 #11/14/21
 
 #problem 3 i will multiply 5,2,7,-1
+'''
+Define function first.
+Then call the function.
+'''
 
 m = [5,2,7,-1]
 print(list(m))
 def multi(m):
-
     x = 1
     for i in m:
         x = x * i
         print(x)
+multi(m)
