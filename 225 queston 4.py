3caba415236841affbb3973ef8a12f965da9cdec
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-11-21 22:34:18 -0600

    Update 225 queston 4.py

--- 225 queston 4.py
+++ 225 queston 4.py
@@ -1,12 +1,15 @@
 #luis rivera
 #11/14/21
-
+'''
+Each element in new list should be unique.
+'''
 #problem 4 is trying to find a unique iteam
 
 def list(l):
     r = [] #r is a list with unique iteams
     for i in l:
-        r.append(i)
+        if i not in r: # if there is no same elelement in the list r
+           r.append(i) #then add it to the list r
     return r
 
 x = [1,3,3,3,6,2,3,5]
